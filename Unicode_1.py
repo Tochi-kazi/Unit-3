524fc4648de971977c0152dd59d36f56c058435e
Tochi <tochukwu.iroakazi@ocsbstudent.ca>
2017-10-23 18:39:57 -0400

    Using Unicode

---
+++ Unicode_1.py
@@ -0,0 +1,14 @@
+# Created by : Tochukwu Iroakazi
+# Created on : oct-2017
+# Created for : ICS3UR-1 
+# Daily Assignment - Unit3-07
+# This program displays second type factorial
+
+import ui
+
+
+for counter in range(65,91):
+    print (unichr(counter) + '->' + str(counter))
+
+for counter in range (97,122):
+    print (unichr(counter) + '->' + str(counter))
