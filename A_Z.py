de43332a3c10b566b09737abfb10a1ab09d80e1b
Tochi <tochukwu.iroakazi@ocsbstudent.ca>
2017-10-26 12:43:51 -0400

    A->Z

---
+++ A_Z.py
@@ -0,0 +1,12 @@
+# Created by : Tochukwu Iroakazi
+# Created on : oct-2017
+# Created for : ICS3UR-1 
+# Daily Assignment - Unit3-09
+# This program displays A to Z in small and big letters
+
+
+for Capitals in range(65,91):
+    for lowercase in range(97,123):
+      print (unichr(Capitals) + '->' + unichr(lowercase))
+
+
